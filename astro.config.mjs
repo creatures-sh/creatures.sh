d0e4fb0d9d2736eb5c72582f6586d81b719523db
Lazar Nikolov <hello@nikolovlazar.com>
2023-07-08 18:46:18 -0400

    astro config netlify settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: netlify()
-});
+  output: 'hybrid',
+  adapter: netlify(),
+})
