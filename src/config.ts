7d915a9e81135b0b7669a3cd642f782b99ba0513
Lazar Nikolov <hello@nikolovlazar.com>
2023-08-12 16:18:09 -0400

    feature(blog): new writing banner + other minor changes

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,9 @@
 export const GITHUB_URL = 'https://github.com/creatures-dev'
 export const DISCORD_URL = 'https://discord.creatures.dev'
+export const CONTRIBUTE_DOC_URL =
+  'https://github.com/creatures-dev/creatures.dev/blob/main/CONTRIBUTING.md'
 export const TWITCH_URL = 'https://twitch.tv/nikolovlazar'
-export const DISCORD_MEMBERS = 70
+export const DISCORD_MEMBERS = 80
+export const STREAM_PROJECT = 'https://github.com/creatures-dev/rss2discord'
 export const LAST_STREAM_VOD =
-  'https://www.youtube-nocookie.com/embed/wsvghzNQyFo'
+  'https://www.youtube-nocookie.com/embed/RuEIhO1jtv8'
