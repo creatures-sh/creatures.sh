479ad581f45766e793e228416419b1f66b5ac5e6
Lazar Nikolov <hello@nikolovlazar.com>
2023-08-14 17:00:49 -0400

    feature(assets): featured images dump

--- src/utils/featured-images.ts
+++ src/utils/featured-images.ts
@@ -1,5 +1,17 @@
-import featured1 from '../assets/blog/featured-1.jpg'
+import type { ImageMetadata } from 'astro'
 
-export const featuredImages: Record<string, ImageMetadata> = {
-  'featured-1.jpg': featured1,
+const imagesImport = import.meta.glob('../assets/blog/*.jpg', {
+  eager: true,
+  import: 'default',
+})
+
+export const featuredImages: Record<string, ImageMetadata> = {}
+
+for (const path in imagesImport) {
+  const name = path.split('/').pop() ?? ''
+  console.log(name)
+  featuredImages[name] = imagesImport[path] as ImageMetadata
 }
+
+console.log('DONE')
+console.log(featuredImages)
