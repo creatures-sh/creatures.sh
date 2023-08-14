bee94af9aafa56515080ecbcc2f9feceeea5ad7a
Andrej Acevski <aacevski@gmail.com>
2023-08-14 22:13:39 +0200

    feature: Updating post schema to accept canonical urls

--- src/content/config.ts
+++ src/content/config.ts
@@ -24,6 +24,7 @@ const blog = defineCollection({
       tags: z.array(z.string()),
       author: reference('authors'),
       excerpt: z.string(),
+      canonicalUrl: z.string().optional(),
     }),
 })
 
