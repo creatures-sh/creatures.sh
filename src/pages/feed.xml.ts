b88bec9b9e049a06c63d29eb3026c9ff3d73b5ee
Lazar Nikolov <hello@nikolovlazar.com>
2024-05-06 15:19:27 -0400

    fixed xml feed 404ing

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -1,18 +1,18 @@
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
-import type { APIContext } from 'astro'
+import type { APIRoute } from 'astro'
 import MarkdownIt from 'markdown-it'
 
 const parser = new MarkdownIt({ html: true })
 
-export async function get(context: APIContext) {
+export const GET: APIRoute = async ({ site }) => {
   const blog = await getCollection('blog')
   const authors = await getCollection('authors')
 
   return rss({
     title: 'creatures.dev',
     description: 'creatures.dev - a community of developers excited about code',
-    site: context.site?.toString() || 'https://creatures.dev',
+    site: site?.toString() || 'https://creatures.dev',
     items: blog.map((post) => ({
       title: post.data.title,
       description: post.data.excerpt,
