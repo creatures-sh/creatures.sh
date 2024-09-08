d700acda8d7781f87995302038e9225cd7d29db0
Lazar Nikolov <hello@nikolovlazar.com>
2024-09-08 11:35:13 -0400

    updating discord members number

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const RSS_URL = 'https://creatures.sh/feed.xml'
 export const CONTRIBUTE_DOC_URL =
   'https://github.com/creatures-sh/creatures.sh/blob/main/CONTRIBUTING.md'
 export const YOUTUBE_URL = 'https://youtube.com/@nikolovlazar'
-export const DISCORD_MEMBERS = 420
+export const DISCORD_MEMBERS = 530
 export const STREAM_PROJECT = 'https://github.com/creatures-sh/rss2discord'
 export const LAST_STREAM_VOD =
   'https://www.youtube-nocookie.com/embed/XYJRXjlHUak'
